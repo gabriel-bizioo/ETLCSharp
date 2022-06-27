d40b07d3a964fca6086f6751a42b724d967e5550
gabriel-bizioo <gabriel.bizio@ufpr.br>
2022-06-27 14:15:33 -0300

    engenharia reversa

---
+++ main/reverseeng.ps1
@@ -0,0 +1,5 @@
+$strconn = "Server=CTPC3628\SQLEXPRESS;Database=ets-dados;Integrated Security=True;"
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+dotnet tool install --global dotnet-ef
+dotnet ef dbcontext scaffold $strconn Microsoft.EntityFrameworkCore.SqlServer
