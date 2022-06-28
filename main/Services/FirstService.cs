4ac8cce4f5052a3762ce3b4442d8b6abd9ea17fe
gabriel-bizioo <gabriel.bizio@ufpr.br>
2022-06-28 16:14:10 -0300

    parei de mexer pq meu projeto ta pegando fogo aqui

--- main/Services/FirstService.cs
+++ main/Services/FirstService.cs
@@ -5,20 +5,60 @@ namespace main.Services;
 
 class FirstService{
     public void NewTable(){
-        
-        int MediaSalarial;
-
-        string Doenca;
-
-        int MediaIdade;
 
         using(var context = new analytic_dataContext())
         {
-            var Media = context.Pacientes.Join(context.ClasseSocials, pc => pc.IdClasseSocial, cs => cs.Id, (pc, cs) => new
+            var Query = context.Diagnosticos.Join(context.Pacientes, dg => dg.IdPaciente, pc => pc.Id, (dg, pc) => new
             {
-                salario = cs.SalarioTeto,
-            });       
+                IdPaciente = pc.Id,
+                IdClasse = pc.IdClasseSocial,
+                IdDoenca = dg.IdDoenca,
+                Idade = pc.Idade
+            }).Join(context.ClasseSocials, dgpc => dgpc.IdClasse, cs => cs.Id, (dgpc, cs) => new
+            {
+                Teto = cs.SalarioTeto,
+                IdDoenca = dgpc.IdDoenca,
+                Idade = dgpc.Idade
+            }).Join(context.Doencas, dgpccs => dgpccs.IdDoenca, dn => dn.Id, (dgpccs, dn) => new
+            {
+                NomeDoenca = dn.Nome,
+                Salario = dgpccs.Teto,
+                Idade = dgpccs.Idade
+            }).GroupBy(x => x.NomeDoenca)
+            .Select(g => new {
+                salariomedio = g.Average(x => x.Salario),
+                idademedia = g.Average(i => i.Idade),
+                Doenca = g.Key
+            });
 
+            using(var context2 = new ets_dadosContext())
+            {
+                var table = context2.NewTables;
+
+                foreach(var line in Query){
+                    var Line  = new NewTable()
+                    {
+                        Doenca = line.Doenca,
+                        MediaSalarial = (int)line.salariomedio,
+                        MediaIdade = (int)line.idademedia
+                    };
+                    
+                    table.Add(Line);
+                }
+            }
+        }
+
+
+    }
+
+    public void DoencaIdadeRegiao()
+    {
+        using(var context = new analytic_dataContext())
+        {
+            var Query = context.Diagnosticos.Join(context.Pacientes, dg => dg.IdPaciente, pc => pc.Id, (pc, dg) => new
+            {
+
+            });
         }
     }
 }
